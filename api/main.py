fadbbd6a3b7dac02ca2137ee82bfa8efb1c1903a
Kevin Wielander <kw@woschitzgroup.com>
2023-02-02 19:05:04 +0100

    Refs #5 added another endpoint for bulk upload  of images

--- api/main.py
+++ api/main.py
@@ -16,19 +16,27 @@ def root(request: Request):
 
 @app.post("/single_file_ocr")
 async def single_file_ocr(image: UploadFile = File(...)):
-    filename = _store_file(image)
+    path = "../temp/single"
+    os.mkdir(path)
+    filename = _store_file(image, path, image.filename)
+    #os.remove(path)
     return {"filename": filename, "text": "this works"}
 
 
 @app.post("/bulk_ocr")
-async def bulk_ocr(request:Request, bgt: BackgroundTasks):
+async def bulk_ocr(images: List[UploadFile] = File(...)):
+    path = "../temp/bulk"
+    os.mkdir(path)
+    for image in images:
+        _store_file(image,path,image.filename)
+    #os.remove(path)
     return {"filename": "filename", "text": "this works"}
 
 
 
-def _store_file(file):
+def _store_file(file, path, name):
     extension = os.path.splitext(file.filename)[-1]
-    temp_file = os.path.join("../temp", "temp" + extension)
+    temp_file = os.path.join(path, name + extension)
     with open(temp_file, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     return temp_file
