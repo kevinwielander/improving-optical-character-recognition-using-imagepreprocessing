dbe6fa3f7343f39ccb53f554ceaa52328604d3d5
Kevin Wielander <kw@woschitzgroup.com>
2023-02-02 18:35:25 +0100

    Refs #5 implemented basic single file upload endpoint

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
 from typing import List
-from fastapi import FastAPI, UploadFile, Request
+from fastapi import FastAPI, UploadFile, Request, File, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 import cv2
 import numpy as np
+import os
+import shutil
 
 app = FastAPI()
 frontend = Jinja2Templates(directory="../frontend")
@@ -12,33 +14,24 @@ frontend = Jinja2Templates(directory="../frontend")
 def root(request: Request):
     return frontend.TemplateResponse("index.html", {"request":request})
 
-@app.post("/preprocess")
-async def preprocess(file: UploadFile):
-    img = cv2.imdecode(np.frombuffer(await file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-     
-     # Convert the image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-   
-    # Encode the grayscale image data into a byte string
-    encoded_img = cv2.imencode('.jpg', gray)[1].tobytes()
+@app.post("/single_file_ocr")
+async def single_file_ocr(image: UploadFile = File(...)):
+    filename = _store_file(image)
+    return {"filename": filename, "text": "this works"}
 
-    # Return the encoded grayscale image data as binary data
-    return {"image": encoded_img, "file_name": file.filename}
 
-@app.post("/ocr")
-async def ocr(files: List[UploadFile]):
-    recognized_texts = []
-    
-    for file in files:
-        # Load the image from the file
-        img = cv2.imread(file.file.name)
-        
-        # Set the Tesseract data path from the environment variable
-        # pytesseract.pytesseract.tesseract_cmd = os.environ.get("TESSERACT_DATA_PATH", "tesseract")
-        
-        # Use Tesseract to perform OCR on the image
-        text = "test return text"
-        
-        recognized_texts.append(text)
-    
-    return {"recognized_texts": recognized_texts}
+@app.post("/bulk_ocr")
+async def bulk_ocr(request:Request, bgt: BackgroundTasks):
+    return {"filename": "filename", "text": "this works"}
+
+
+
+def _store_file(file):
+    extension = os.path.splitext(file.filename)[-1]
+    temp_file = os.path.join("../temp", "temp" + extension)
+    with open(temp_file, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return temp_file
+
+
+
