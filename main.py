44fc28e0af17b935a15ab4a05745dab1d5f49098
Kevin Wielander <kw@woschitzgroup.com>
2023-07-05 09:10:32 +0200

    fix: added favicon to get ridf of 404 b not found on serving index.html

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import asyncio
+
+from starlette.responses import FileResponse
 from typing import List
 from fastapi import FastAPI, UploadFile, Request, File, Response, HTTPException
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from io import BytesIO
 import os
 import shutil
@@ -10,10 +13,13 @@ from pdf2image import convert_from_path
 
 from ocr.tesseract import read_image
 from preprocessing.preprocessor import Preprocessor
+from quality_metrics.text_metrics_report import TextMetricsReport
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
 frontend = Jinja2Templates(directory="frontend")
 path = "temp"
+
 
 @app.get("/")
 def root(request: Request):
@@ -58,11 +64,34 @@ async def bulk_ocr(images: List[UploadFile] = File(...)):
     return results
 
 
+@app.post("/text_metrics")
+async def text_metrics_report(ocr_files: List[UploadFile] = File(...), gt_files: List[UploadFile] = File(...)):
+    if len(ocr_files) == 0 or len(gt_files) == 0:
+        raise HTTPException(status_code=400, detail="No text files provided")
+
+    ocr_texts = []
+    ground_truths = []
+    for ocr_file, gt_file in zip(ocr_files, gt_files):
+        ocr_filename = _store_file(ocr_file, ocr_file.filename)
+        gt_filename = _store_file(gt_file, gt_file.filename)
+
+        with open(ocr_filename, 'r') as f:
+            ocr_texts.append(f.read())
+        ground_truths.append(gt_filename)
+
+    report = TextMetricsReport(ground_truths, ocr_texts)
+    report.generate_report()
+
+    for ocr_file, gt_file in zip(ocr_files, gt_files):
+        os.remove(ocr_file.filename)
+        os.remove(gt_file.filename)
+
+    return FileResponse(report.filename, media_type='text/csv',
+                        headers={"Content-Disposition": f"attachment;filename={report.filename}"})
+
+
 def _store_file(file, name):
     temp_file = os.path.join(path, name)
     with open(temp_file, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     return temp_file
-
-
-
--- quality_metrics/text_metrics.py
+++ quality_metrics/text_metrics.py
@@ -1,10 +1,18 @@
+import os
+
 import nltk
 
 
 class TextMetrics:
-    def __init__(self, ground_truth, ocr_text):
-        self.ground_truth = ground_truth
+    def __init__(self, filename, ocr_text):
+        self.filename = filename
         self.ocr_text = ocr_text
+        self.ground_truth = self._read_ground_truth()
+
+    def _read_ground_truth(self):
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(base_path, 'resources', self.filename), 'r') as f:
+            return f.read()
 
     def wer(self):
         return nltk.edit_distance(self.ground_truth.split(), self.ocr_text.split()) / len(self.ground_truth.split())
